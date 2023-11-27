dd798d913c040279316004770747f8bf2c251798
Arif Daud <arif.daud@aemenersol.com>
2023-11-27 15:10:14 +0800

    new changes (remove countdown)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import { PostDetailComponent } from './post-detail/post-detail.component';
 import { TodoComponent } from './todo/todo.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { CountdownComponent } from './countdown/countdown.component';
+
 
 @NgModule({
   declarations: [
@@ -24,8 +24,7 @@ import { CountdownComponent } from './countdown/countdown.component';
     WelcomeComponent,
     PostDetailComponent,
     TodoComponent,
-    NavbarComponent,
-    CountdownComponent,
+    NavbarComponent
   ],
 
   imports: [
